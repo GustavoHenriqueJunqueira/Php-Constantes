15b46b6d0c39200a10a507a47cb621f4b480b98b
Gustavo Henrique F. Junqueira <gustavohenriquejunqueira@hotmail.com>
2019-06-03 18:10:29 -0300

    Update exemplo-02.php

--- exemplo-02.php
+++ exemplo-02.php
@@ -5,8 +5,11 @@ define("BANCO_DE_DADOS",[
 	'root',
 	'password',
 	'teste'
-],true); //Este true colocado no final do define é para o node ser insensitive, desta forma o nome banco de dados no superior está com maiusculo e ao dar o print está digitado tudo minusculo. desta forma não dará erro quando for executado. 
-//Mas por boas praticas deve-se sempre deixar tudo maiusculo.
+],true);
+/**Este true colocado no final do define é para o node ser insensitive,
+desta forma o nome banco de dados no superior está com maiusculo e ao dar
+o print está digitado tudo minusculo. desta forma não dará erro quando for executado. 
+Mas por boas praticas deve-se sempre deixar tudo maiusculo.**/
 
 print_r(banco_de_dados);
 
